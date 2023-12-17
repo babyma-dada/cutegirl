e6c4016a74d4d9d9770df248fe12166d373cb1fd
babyma-dada <144602853+babyma-dada@users.noreply.github.com>
2023-12-17 16:38:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", "6009759960:AAGqtFp6A3Wynna1oE9FCxKFcxAapQJlOso")
+TOKEN = getenv("TOKEN", "6009759960:AAHn-y7fJNzSkmZSZQ4y8grm5tmDlCikbVM")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
